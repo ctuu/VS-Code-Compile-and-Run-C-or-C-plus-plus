862ff7f01a76f804bd5e8a581fb1146fe416753f
ctuu <ctuu1s@outlook.com>
2019-03-23 17:06:36 +0800

    initial.

---
+++ README.md
@@ -0,0 +1,1 @@
+[VS Code 搭建 C/C++ 编译运行环境的四种方案](https://zhuanlan.zhihu.com/p/35178331/)
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cout << "Hello World" << endl;
+    return 0;
+}
